88e8f76770481671da4ca5c87e49d8f9d1f1c994
cotsdev93 <cotsdev93@gmail.com>
2025-01-28 12:47:13 -0300

    agregado de animacion de intro main

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-
 function mainAnimation() {
-    const logo = document.querySelector(".logo")
-    const language = document.querySelector(".language")
-    const moon = document.querySelector(".fa-moon")
-    const menu = document.querySelector(".fa-bars")
-    logo.classList.add("intro")
-    setTimeout(() => {
-       menu.classList.add("intro") 
-    }, 500);
-    setTimeout(() => {
-       language.classList.add("intro") 
-    }, 700);
-    setTimeout(() => {
-        moon.classList.add("intro")
-    }, 1000);
-    console.log("funca")
+  const logo = document.querySelector(".logo");
+  const language = document.querySelector(".language");
+  const moon = document.querySelector(".fa-moon");
+  const menu = document.querySelector(".fa-bars");
+  logo.classList.add("intro");
+  setTimeout(() => {
+    menu.classList.add("intro");
+  }, 500);
+  setTimeout(() => {
+    language.classList.add("intro");
+  }, 700);
+  setTimeout(() => {
+    moon.classList.add("intro");
+  }, 1000);
+
+  const moreInfo = document.querySelector(".moreInfo");
+  const presentation = document.querySelector(".presentation");
+  const welcomeMessage = document.querySelector(".welcomeMessage");
+  setTimeout(() => {
+    moreInfo.classList.add("intro");
+  }, 1100);
+  setTimeout(() => {
+    presentation.classList.add("intro");
+  }, 1300);
+  setTimeout(() => {
+    welcomeMessage.classList.add("intro");
+  }, 1600);
 }
-mainAnimation()
+mainAnimation();
